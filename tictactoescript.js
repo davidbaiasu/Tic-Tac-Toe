40333c7fc705e9262d210ee8f7596dd0a40b76c4
davidbaiasu <baiasudavidalin@gmail.com>
2025-11-19 20:30:33 +0200

    Added checkWinner

--- tictactoescript.js
+++ tictactoescript.js
@@ -1,9 +1,25 @@
 const cells = document.querySelectorAll("#grid td");
+
+const turn_header = document.getElementById("turn");
 const turn_span = document.getElementById("turn_span");
 
 let turn = 0;
 // turn is even => x's turn
 // turn is odd 	=> o's turn
+
+let board = ["", "", "", "", "", "", "", "", ""];
+
+const winning_combos = [
+    [0, 1, 2], // top row
+    [3, 4, 5], // middle row
+    [6, 7, 8], // bottom row
+    [0, 3, 6], // left column
+    [1, 4, 7], // middle column
+    [2, 5, 8], // right column
+    [0, 4, 8], // diagonal top-left → bottom-right
+    [2, 4, 6]  // diagonal top-right → bottom-left
+];
+
 
 cells.forEach((cell, index) => {
 	
@@ -17,6 +33,7 @@ cells.forEach((cell, index) => {
 			
 			img.src = "x.jpg";
 			turn++;
+			board[index] = "x";
 			
 		}
 		
@@ -24,6 +41,21 @@ cells.forEach((cell, index) => {
 			
 			img.src = "o.jpg";
 			turn++;
+			board[index] = "o";
+			
+		}
+		
+		let result = checkWinner();
+		
+		if( result === "x" ){
+			
+			turn_header.textContent = "X wins !!!"; 
+			
+		}
+		
+		else if( result === "o" ){
+			
+			turn_header.textContent = "O wins !!!"; 
 			
 		}
 		
@@ -36,10 +68,33 @@ cells.forEach((cell, index) => {
 function changeTurn(turn){
 	
 	if( turn % 2 == 0 ){
+		
 		turn_span.textContent = "X";
+		
 	}
 	else{
+		
 		turn_span.textContent = "O";
+		
 	}
 	
 }	
+
+function checkWinner(){
+	
+	for( const combo of winning_combos ){
+		
+		const [a, b, c] = combo;
+		
+		if( board[a] != "" && board[a] === board[b] && board[a] === board[c]){
+			
+			console.log("win");
+			return board[a];
+		
+		}
+		
+	}
+	
+	return null;
+	
+}
